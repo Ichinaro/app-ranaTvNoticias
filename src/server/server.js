3ad4d0afd19149094ca9dccfcc4acd6696fea6d7
Ichinaro <65051884+Ichinaro@users.noreply.github.com>
2022-01-30 22:51:17 +0100

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -60,6 +60,14 @@ const setResponse = (html, preloadedState, manifest) => {
     <!DOCTYPE html>
     <html lang="en">
       <head">
+        <title>RanaTv</title>
+        <meta property="og:url" content="https://ranatv.herokuapp.com" />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="RanaTV"/>
+        <meta property="og:description" content="Noticias"/>
+        <meta property="og:image" content="https://scontent.fmch2-1.fna.fbcdn.net/v/t39.30808-6/229798347_4853783284636688_5860767665755358455_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=e3f864&_nc_ohc=diFpyMDm5ZQAX-BVCoS&_nc_ht=scontent.fmch2-1.fna&oh=00_AT-p26lPvLCHhCDN9G_dyqyP1u2yRCGPyNVEwKbIi8q3LA&oe=61FB2877" />
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
         <link rel="stylesheet" href="${mainStyles}" type="text/css"/>
       </head>
     
