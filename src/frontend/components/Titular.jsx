2cb6b5ec5962e2302ec33d091f3af35dae095b88
Ichinaro <65051884+Ichinaro@users.noreply.github.com>
2022-01-30 20:07:23 +0100

    Update Titular.jsx

--- src/frontend/components/Titular.jsx
+++ src/frontend/components/Titular.jsx
@@ -35,12 +35,12 @@ function Titular(props) {
           </div>
           <div className="publicidad">
             <a href="https://www.facebook.com/PererosDent-103648597933382" >
-              <img src="https://i.postimg.cc/x8hDQbFs/Publicidad1.jpg" alt=""/>
+              <img src="https://scontent.fmch2-1.fna.fbcdn.net/v/t1.6435-9/77017767_3104120362936331_194080191897665536_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=e3f864&_nc_ohc=RDsIUXv3vnoAX-yCFWX&_nc_ht=scontent.fmch2-1.fna&oh=00_AT_q_fySwDvykNU_iO4IradyBJpvPrFhyqx6fPcp_lDIwg&oe=621CB0F3" alt=""/>
             </a>
           </div>
           <div className="publicidad">
             <a href="https://www.facebook.com/PererosDent-103648597933382" >
-              <img src="https://i.postimg.cc/x8hDQbFs/Publicidad1.jpg" alt=""/>
+              <img src="https://scontent.fmch2-1.fna.fbcdn.net/v/t1.6435-9/84327682_3275091669172532_6632766605652656128_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=e3f864&_nc_ohc=VikE-RUzzxQAX_t4aXx&_nc_ht=scontent.fmch2-1.fna&oh=00_AT-p23RsFB4rEGe4b2Z7YgX467VjTRgcIGOLDOMCrRs5mg&oe=621DF329" alt=""/>
             </a>
           </div>
         </div>
